a861cf969ef54d41db5b88a5bdce81ac299ae4c1
Tanner Perry <tc-boy@hotmail.com>
2021-09-22 18:09:42 -0600

    final commit

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -274,7 +274,7 @@ console.log(totalFrogs)
 
 
 ////////////////// PROBLEM 15 ////////////////////
-let sampleArray = [0,1,2,3,4,7,5,6,8,9]
+let sampleArray = [0,1,2,3,4,5,6,7,8,9]
 /*
   Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
 */
@@ -284,15 +284,19 @@ function sortedArr(arr){
 
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] < arr[i + 1]) {
-      arrIsSorted;
+      console.log(sampleArray[i],"&",sampleArray[i + 1],"is in order")
+    } else if (arr[i] > arr[i + 1]) {
+      console.log(sampleArray[i],"&",sampleArray[i + 1],"are not in order")
+      return arrIsSorted = false
     } else {
-      !(arrIsSorted)
+      console.log("Array is in order")
+      return arrIsSorted
     }
   }
+  console.log(arrIsSorted)
 }
 
-let arrayIsAscending = sortedArr(sampleString);
-console.log(arrayIsAscending)
+console.log(sortedArr(sampleArray))
 
 
 ////////////////// PROBLEM 16 ////////////////////
@@ -317,13 +321,13 @@ function pond() {
 */
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = []
+let globalScope = ["duck"]
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = []
+let bathroomScope = ["rubber duck"]
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = []
+let bathtubScope = ["sailor duck"]
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = []
+let pondScope = ["real duck"]
